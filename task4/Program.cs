0cc88f872688f85fcffaad483224785d0c82a318
Marina <mv.nikolaeva1507@gmail.com>
2022-08-05 13:05:53 +0300

    add in task4 2 examples

--- task4/Program.cs
+++ task4/Program.cs
@@ -5,6 +5,24 @@
 // 782 -> 2
 // 918 -> 8
 
+// Console.WriteLine("Введите трехзначное число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Последняя цифра числа {a}={a%10}");
+
+// int a;
+// do
+// {
+// Console.WriteLine("Введите трехзначное число: ");
+// a = Convert.ToInt32(Console.ReadLine());
+// }
+// while (a <= 99 || a > 999);
+// Console.WriteLine($"Последняя цифра числа {a}={a%10}");
+
 Console.WriteLine("Введите трехзначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Последняя цифра числа {a}={a%10}");
+if (a >= 100 && a < 1000)
+{
+    Console.WriteLine($"Последняя цифра числа {a}={a%10}");
+}
+else
+Console.WriteLine("Вы ввели не трехзначное число. Повторите попытку");
