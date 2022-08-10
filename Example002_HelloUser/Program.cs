786ba73c04a626a34526bb04024a33fc5d0eb3ab
Marina <mv.nikolaeva1507@gmail.com>
2022-08-10 12:36:38 +0300

    correct Example 002 HelloUser

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,3 @@
-﻿Console.Write("Введите ваше имя" );
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username); 
+﻿Console.WriteLine("Введите ваше имя: ");
+string? userName = Console.ReadLine();
+Console.Write($"Привет, {userName}!");
