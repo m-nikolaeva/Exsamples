5b3dad5414b1df156f85d7f845c84b190350648d
Marina <mv.nikolaeva1507@gmail.com>
2022-08-16 12:42:21 +0300

    add task 15 sem4

---
+++ task15_sem4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая
+// 1. принимает на вход число и 
+// 2. выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Веедите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NumCount(int number)
+{
+    int sum = 0;
+    for (int i = 1; number > 0; i++)
+    {
+        number = number / 10;
+        sum = i;
+    }
+    return sum;
+}
+
+int res = NumCount(num);
+Console.WriteLine($"Количество цифр в числе {num} = {res}");
