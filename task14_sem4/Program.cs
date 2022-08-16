f48b34d401e54d97c319c5b2346ee0d061c5b634
Marina <mv.nikolaeva1507@gmail.com>
2022-08-16 12:40:59 +0300

    add task 14 sem 4

---
+++ task14_sem4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 24: Напишите программу, которая
+// 1. принимает на вход число (А) и 
+// 2.выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+// for (int i = 1; i <= num; i++)
+// {
+//    sum +=i;  //sum = sum + i;
+// }
+
+// Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
+
+
+// Метод:
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum +=i;  //sum = sum + i;
+    } 
+    return sum;
+}
+
+int result = SumNumbers(num);
+Console.WriteLine($"Сумма чисел от 1 до {num} = {result}");
