bbc7351b9755f7c51d360d9a0e0717320222c64f
Marina <mv.nikolaeva1507@gmail.com>
2022-08-16 12:42:56 +0300

    add task 16 sem4

---
+++ task16_sem4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите целое положительное число: ");
+long num = Convert.ToInt64(Console.ReadLine());
+
+long GetFact(long number)
+{
+    long fact = 1;
+    for (long i = 1; i <= number; i++)
+    {
+        fact*=i; //fact = fact*i
+    }
+    return fact;
+}
+
+long res = GetFact(num);
+Console.WriteLine($"Произведение чисел от 1 до{num} -> {res}");
