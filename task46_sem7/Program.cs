febd135e25ecc217116dc4443ee7f85696f2c5c6
Marina <mv.nikolaeva1507@gmail.com>
2022-08-26 15:14:27 +0300

    add task 46 sem 7

---
+++ task46_sem7/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)  //метод создания двумерного массива
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+
+void PrintMatrix(int[,] matrix)    //метод вывода в консоль двумерного массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4},");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -9, 9);
+PrintMatrix(array2D);
+
+
+
