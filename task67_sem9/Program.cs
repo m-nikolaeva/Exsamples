20ebe59ab1f95ced5d2ee4f41f8c9a299757b737
Marina <mv.nikolaeva1507@gmail.com>
2022-09-02 11:33:29 +0300

    add task 63 sem 9

---
+++ task67_sem9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите целое положительное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = DigitsSum(m);
+
+Console.WriteLine($"Сумма цифр = {sum}");
+
+int DigitsSum(int number)
+{
+	if (number < 10)
+		return number;
+	return number % 10 + DigitsSum(number / 10);
+}
